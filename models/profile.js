da85dfb1d199b52d5927673ad36c5e3d4b9a67a3
GFou <gfouchard20@outlook.com>
2022-10-21 17:23:12 -0400

    add dreamcast ref to profileSchema

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  dreamCast: [{type: Schema.Types.ObjectId, ref: 'Dreamcast'}],
 },{
   timestamps: true,
 })
