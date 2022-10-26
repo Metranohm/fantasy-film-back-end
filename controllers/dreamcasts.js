8cae556d72c3cb8954c255d31d28578048ea1130
GFou <gfouchard20@outlook.com>
2022-10-26 11:56:07 -0400

    editdreamcast controller create function

--- controllers/dreamcasts.js
+++ controllers/dreamcasts.js
@@ -1,17 +1,24 @@
 import { Profile } from "../models/profile.js"
 import { Dreamcast } from "../models/dreamcast.js"
+import { Actor } from "../models/actor.js"
 
 const create = async (req, res) => {
   try {
     req.body.author = req.user.profile
-    const dreamcast = await Dreamcast.create(req.body)
-    const profile = await Profile.findByIdAndUpdate(
-      req.user.profile,
-      { $push: { dreamcasts: dreamcast } },
-      { new: true }
-    )
-    dreamcast.author = profile
-    res.status(201).json(dreamcast)
+    // const dreamcast = await Dreamcast.create(req.body)
+    
+    
+    for (let cast = 0; cast < req.body.cast.length; cast++ ) {
+      const actor = await Actor.findOne({'tmdbID': `${req.body.cast[cast].actors}`})
+      console.log(actor.name, req.body.cast[cast].character)
+    }
+    // const profile = await Profile.findByIdAndUpdate(
+    //   req.user.profile,
+    //   { $push: { dreamcasts: dreamcast } },
+    //   { new: true }
+    // )
+    // dreamcast.author = profile
+    // res.status(201).json(dreamcast)
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
